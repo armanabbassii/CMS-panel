c11057de7c7f7a5b8ec068959a9b065a904adcbe
Arman Abbasi <arman.abbasi.nt@gmail.com>
2022-12-16 17:58:16 +0330

    locators changes

---
+++ Locators.py
@@ -0,0 +1,10 @@
+#///////////////////login/////////////////
+username_button_class = "(//div[@class = 'login-btn'])[1]"
+
+#//////////////////SSO login//////////////
+username_textbox_name   = "//*[@name='identity']"
+password_textbox_name   = "//*[@name='password']"
+enter_button_name   = "(//*[@name='go'])[1]"
+users_button_title  = "//*[@title= 'seller.cms3']"
+
+#//////////////////first page of panel//////////////
