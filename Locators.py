6d19eab827bcd72fcea1c7d3033bbb9c5060f2bc
Arman Abbasi <arman.abbasi.nt@gmail.com>
2022-12-16 23:54:43 +0330

    locators changes

--- Locators.py
+++ Locators.py
@@ -12,3 +12,10 @@ contenttype_dropdown_class  = "(//*[@class = 'nav-link nav-dropdown-toggle'])[8]
 
 #//////////////////create content-type//////////////
 createcontenttype_button_text   = "//*[text()= 'افزودن نوع محتوا']"
+
+type_button_id  = "//*[@id= 'react-select-2-input']"
+ig_button_id    = "//*[@id= 'react-select-3-input']"
+name_textbox_name   ="//*[@name= 'name']"
+entityId_textbox_name   = "//*[@name= 'code']"
+description_textbox_name    = "(//*[@name= 'description'])[2]"
+submit_button_text  = "//*[text()= 'ذخیره']"
