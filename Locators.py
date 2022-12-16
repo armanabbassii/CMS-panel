144423127d3559de25499f059b9f734e436c685f
Arman Abbasi <arman.abbasi.nt@gmail.com>
2022-12-16 19:30:13 +0330

    locators changes

--- Locators.py
+++ Locators.py
@@ -8,3 +8,7 @@ enter_button_name   = "(//*[@name='go'])[1]"
 users_button_title  = "//*[@title= 'seller.cms3']"
 
 #//////////////////first page of panel//////////////
+contenttype_dropdown_class  = "(//*[@class = 'nav-link nav-dropdown-toggle'])[8]"
+
+#//////////////////create content-type//////////////
+createcontenttype_button_text   = "//*[text()= 'افزودن نوع محتوا']"
