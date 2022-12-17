e4f284dba6dcb1f8779d53c1174867a9629de778
armanabbassii <arman.abbasi.nt@gmail.com>
2022-12-17 16:25:19 +0330

    page => class Login

--- Pages/Login.py
+++ Pages/Login.py
@@ -1,1 +1,11 @@
-#ffsafsd
+from Locators import *
+
+
+class Login:
+
+    def __int__(self, driver):
+        self.driver = driver
+
+    def login_with_username(self):
+        self.driver.find_element('xpath', username_button_class).click()
+
