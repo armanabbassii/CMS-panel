640ba3ed0ef7bcd09d9e3aeea42bc06e7d47af61
armanabbassii <arman.abbasi.nt@gmail.com>
2022-12-18 15:58:35 +0330

    page => SSOLogin

---
+++ Tests/LoginTests.py
@@ -0,0 +1,11 @@
+from Pages.Login import Login
+from Pages.SSOLogin import SSOLogin
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
+import unittest
+
+class LoginTests(unittest.TestCase):
+
+
+
