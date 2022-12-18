8edb28c8d62b8a8e50752dadf002d1a59411c36a
armanabbassii <arman.abbasi.nt@gmail.com>
2022-12-18 16:33:44 +0330

    page => SSOLogin

--- Tests/LoginTests.py
+++ Tests/LoginTests.py
@@ -5,6 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
 import unittest
 
+
 class LoginTests(unittest.TestCase):
 
     @classmethod
@@ -12,6 +13,8 @@ class LoginTests(unittest.TestCase):
         cls.driver = webdriver.Chrome(executable_path=ChromeDriverManager().install())
         cls.driver.implicitly_wait(3)
 
-
-
-
+    def valid_login_vs_SSOLogin_tests(self):
+        self.driver.get('http://dev.cms.test/')
+        login = Login(driver=self.driver)
+        login.login_with_username()
+        sleep(5)
